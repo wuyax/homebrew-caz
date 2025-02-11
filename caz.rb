e18c751a0cc9086e9d7bc370f62149a3380a1581
GitHub Action <action@github.com>
2025-02-11 06:46:24 +0000

    Update formula to version 1.1.1

--- caz.rb
+++ caz.rb
@@ -1,8 +1,8 @@
 class Caz < Formula
   desc "Clean .DS_Store files and create ZIP archives"
   homepage "https://github.com/wuyax/caz"
-  url "https://github.com/wuyax/caz/releases/download/v1.1.0/caz-1.1.0.tar.gz"
-  sha256 "5474ec50535c6d29fd3bbc235e4f58de67b0e8b576cdbb74be8b8289a0942010"
+  url "https://github.com/wuyax/caz/releases/download/v1.1.1/caz-1.1.1.tar.gz"
+  sha256 "88837acf4ff7ab4aaf6fb7a6b7d4a45ade93bdf98446e44c215999da50970cee"
   license "MIT"
 
   def install
